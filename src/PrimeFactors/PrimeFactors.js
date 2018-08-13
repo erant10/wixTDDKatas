d7d64b122fc30a301ab20c67277ea91858c0fdc8
Eran <erant10@gmail.com>
2018-08-13 20:06:05 +0300

    PrimeFactor: trasformation 3 - (unconditional → if) + (statement-> recursion)

--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -1,5 +1,8 @@
 function generate(number) {
     if (number === 1) return [];
+    if (number % 2 === 0) {
+        return [2].concat(generate(number / 2))
+    }
     return [number];
 }
 
--- src/PrimeFactors/PrimeFactors.test.js
+++ src/PrimeFactors/PrimeFactors.test.js
@@ -4,13 +4,23 @@ const PrimeFactors = require('./PrimeFactors');
 describe('PrimeFactors', () => {
     describe('generate', () => {
 
-        it('null → constant', (done) => {
+        it('1. null → constant', (done) => {
             assert.deepEqual(PrimeFactors.generate(1),[]);
             done();
         });
 
-        it('unconditional → if', (done) => {
+        it('2. unconditional → if', (done) => {
             assert.deepEqual(PrimeFactors.generate(2),[2]);
+            assert.deepEqual(PrimeFactors.generate(3),[3]);
+            done();
+        });
+
+        it('3. (unconditional → if) + (statement-> recursion)', (done) => {
+            assert.deepEqual(PrimeFactors.generate(4),[2,2]);
+            assert.deepEqual(PrimeFactors.generate(5),[5]);
+            assert.deepEqual(PrimeFactors.generate(6),[2,3]);
+            assert.deepEqual(PrimeFactors.generate(7),[7]);
+            assert.deepEqual(PrimeFactors.generate(8),[2,2,2]);
             done();
         });
 
