30deb7b9aa6c59a61cc381f28cea3023eccbec1b
Eran <erant10@gmail.com>
2018-08-14 19:27:16 +0300

    Bowling Test 4: Roll a strike

--- src/BowlingGame/BowlingGame.js
+++ src/BowlingGame/BowlingGame.js
@@ -15,14 +15,35 @@ class Game {
     score() {
         let gameScore = 0;
         for (let frame = 0; frame < 10; frame++) {
-            let frameScore = this.rolls[2*frame] + this.rolls[2*frame+1];
+            let frameScore = this._getFrameScore(frame);
             if (frameScore === 10) {
-                // SPARE: add the score of the first roll in the next frame
-                frameScore += this.rolls[2*frame+2]
+                if (this._isStrike(frame)) {
+                    // STRIKE: add the score of the the next frame
+                    frameScore += this._getStrikeBonus(frame);
+                } else {
+                    // SPARE: add the score of the first roll in the next frame
+                    frameScore += this._getSpareBonus(frame);
+                }
             }
             gameScore += frameScore;
         }
         return gameScore;
+    }
+
+    _getFrameScore(frame) {
+        return this.rolls[2*frame] + this.rolls[2*frame+1];
+    }
+
+    _getStrikeBonus(frame) {
+        return this.rolls[2*frame+2] + this.rolls[2*frame+3];
+    }
+
+    _getSpareBonus(frame) {
+        return this.rolls[2*frame+2];
+    }
+
+    _isStrike(frame) {
+        return this.rolls[2*frame] === 10;
     }
 }
 
--- src/BowlingGame/BowlingGame.test.js
+++ src/BowlingGame/BowlingGame.test.js
@@ -22,20 +22,44 @@ describe('BowlingGameTest', () => {
     });
 
     it('roll one spare', done => {
-        g.roll(5);
-        g.roll(5);
+        rollSpare();
         g.roll(3);
         rollN(17,0);
         assert.equal(g.score(), 16);
         done();
     });
 
+    it('roll a strike', done => {
+        rollStrike();
+        let firstRoll = getRandomInt(0,9),
+            secondRoll = getRandomInt(0,9);
+        g.roll(firstRoll);
+        g.roll(secondRoll);
+        rollN(16,0);
+        assert.equal(g.score(), 10 + 2*(firstRoll + secondRoll));
+        done();
+    });
+
 
 });
 
+function rollSpare() {
+    let firstRoll = getRandomInt(0,9)
+    g.roll(firstRoll);
+    g.roll(10-firstRoll);
+}
+
+function rollStrike() {
+    g.roll(10);
+    g.roll(0);
+}
 
 function rollN(n,pins) {
     for (let i=0; i<n; i++) {
         g.roll(pins);
     }
+}
+
+function getRandomInt(min,max) {
+    return Math.floor(Math.random() * (max-min+1) + min);
 }
